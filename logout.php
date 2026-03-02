bec35678e3b92d181188bd71209aac054420c16c
HimaPH05 <himaph05@gmail.com>
2026-03-02 23:37:07 +0530

    added logout php file

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+session_destroy();
+
+echo "<script>
+        alert('Logged out successfully!');
+        window.location.href='login.html';
+      </script>";
+exit();
+?>
