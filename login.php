6c1d2561fd28d1573346b7df295eb644b02bad4b
Hari-005 <sreehari1845@gmail.com>
2026-03-09 15:32:04 +0530

    login ui

--- login.php
+++ login.php
@@ -22,8 +22,9 @@ if (isset($_SESSION["role"])) {
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Placement Hub Login</title>
-<link rel="stylesheet" href="login.css?v=2">
+<link rel="stylesheet" href="login.css?v=3">
 </head>
 
 <body>
